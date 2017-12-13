0360d41bc591a5cd7edd5d2a9e234568206526e3
luis40895 <luis40895@gmail.com>
2017-12-13 12:37:04 +0100

    twitterConfig file.

--- app/tweeterModule.js
+++ app/tweeterModule.js
@@ -1,10 +1,11 @@
 var Twit = require('twit');
+var twitterConfig = require('../twitterConfig');
 
 var T = new Twit({
-    consumer_key: 'SNewHsSpDg3MEzbDIzEeCWXxH',
-    consumer_secret: 'R2lC8JMVPGEJpF75Fg5qXMLX9SwdgaSIMGPkxGBYceUQigDgxr',
-    access_token: '515784347-rBxjQre9Rr9lhunbj62I63f4j3H2PKkyZiSpI1Bz',
-    access_token_secret: 'APx2Prh8pxeXTeIvGdFbuWNb3nz0DjTtze3ozOX2o4IFK'
+    consumer_key: twitterConfig.consumer_key,
+    consumer_secret: twitterConfig.consumer_secret,
+    access_token: twitterConfig.access_token,
+    access_token_secret: twitterConfig.access_token_secret
 });
 
 /*T:get retourne les tweets
