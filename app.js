5049552684dc8ae46fc2fafa60f670a8cd16992b
Stéphan Berchery <stephan.berchery@gmail.com>
2017-12-13 09:30:24 +0100

    init

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var Twit = require('twit')
+var Twit = require('twit');
 
 var T = new Twit({
         consumer_key: 'SNewHsSpDg3MEzbDIzEeCWXxH',
         consumer_secret: 'R2lC8JMVPGEJpF75Fg5qXMLX9SwdgaSIMGPkxGBYceUQigDgxr',
         access_token: '515784347-rBxjQre9Rr9lhunbj62I63f4j3H2PKkyZiSpI1Bz',
         access_token_secret: 'APx2Prh8pxeXTeIvGdFbuWNb3nz0DjTtze3ozOX2o4IFK'
-})
+});
 
 var redis = require("redis"),
     client = redis.createClient();
